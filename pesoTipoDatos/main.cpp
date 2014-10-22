9747f11510852dfb1f25fcdea7dc95b90f1dd99c
Jose Luis Jimenez <joseluiszero@gmail.com>
2014-10-22 19:23:57 +0200

    Uploading vars

---
+++ pesoTipoDatos/main.cpp
@@ -0,0 +1,29 @@
+/** Small program that shows the size in memory of each variable type
+ *	@author Jose Luis Jimenez Urbano | < iJos >
+ *	@date 22/10/2014
+ */
+
+
+#include <stdio.h>
+#include <conio.h>
+
+
+
+int main(){
+	
+	printf("Short int: %d \n", sizeof(short int));
+	printf("Unsigned short int: %d \n", sizeof(unsigned short int));
+	printf("Unsigned int: %d \n", sizeof(unsigned int));
+	printf("Int: %d \n", sizeof(int));
+	printf("Long int: %d \n", sizeof(long int));
+	printf("Signed char: %d \n", sizeof(signed char));
+	printf("Unsigned char: %d \n", sizeof(unsigned char));
+	printf("Float: %d \n", sizeof(float));
+	printf("Double: %d \n", sizeof(double));
+	printf("Long double: %d \n", sizeof(long double));
+
+
+
+	getch();
+	return 0;
+}
